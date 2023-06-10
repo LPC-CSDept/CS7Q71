b4544909754f0ae1857dda2aca56253df4f02dcb
Kyu Lee <kwooler@gmail.com>
2023-06-09 23:49:01 -0700

    Q71

--- main_test.py
+++ main_test.py
@@ -7,37 +7,18 @@ import types
 
 
 def test_main_1():
-    # captureOut = io.StringIO()
-    # sys.stdout = captureOut
-    # datastr = '35 5 10 20 40 15'
-    # sys.stdin = io.StringIO(datastr)
-
-    # main.main()
-    # sys.stdout = sys.__stdout__
-    # print('Captured ', captureOut.getvalue())
-    # lines = captureOut.getvalue().split('\n')
-    # print(lines)
-
     main.main()
     students = main.makeList()
     assert len(students) == 5
     main.printList(students)
 
-    ret = main.scorebySubject(students)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
-
 
 def test_main_2():
     students = main.makeList()
     ret = main.scorebySubject(students)
+    keys = list(ret.keys())
+    print(keys)
+    assert keys == ['1st', '2nd', '3rd', '4th'], "Key must be ['1st', '2nd', '3rd', '4th'] "
     assert len(ret) == 4
     assert sum(ret['1st']) == 490
     assert sum(ret['2nd']) == 430
